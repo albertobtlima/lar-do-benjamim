0b9b4cf62ae501a4fca17aace95d532a3cfe07cb
Diogo Araújo <diogo.araujoctt@gmail.com>
2025-07-13 09:23:13 -0300

    Adicionando a página de voluntário para validação

---
+++ script.js
@@ -0,0 +1,70 @@
+function isHomePage() {
+  const pathname = window.location.pathname.toLowerCase();
+  return (
+    pathname === "/" ||
+    pathname.endsWith("index.html") ||
+    pathname.includes("/home") ||
+    pathname.includes("/inicio")
+  );
+}
+
+document.querySelectorAll("[data-include]").forEach(async (el) => {
+  const file = el.getAttribute("data-include");
+  const res = await fetch(file);
+  const html = await res.text();
+  el.innerHTML = html;
+
+  // Carrossel
+  const carousels = el.querySelectorAll(".carousel");
+  carousels.forEach((carouselEl) => {
+    new bootstrap.Carousel(carouselEl, {
+      interval: 3000,
+      ride: "carousel",
+    });
+  });
+
+  // Reiniciar vídeos em todos os modais de vídeo
+  const modaisVideo = el.querySelectorAll(".modal[id^='modal-video-']");
+  modaisVideo.forEach((modal) => {
+    const iframe = modal.querySelector("iframe");
+    const originalSrc = iframe?.src || "";
+
+    modal.addEventListener("hidden.bs.modal", () => {
+      iframe.src = "";
+    });
+
+    modal.addEventListener("show.bs.modal", () => {
+      iframe.src = originalSrc;
+    });
+  });
+
+  // <h1> Na home
+  if (isHomePage()) {
+    const logo = el.querySelector("#site-logo");
+    if (logo && !logo.closest("h1")) {
+      const h1 = document.createElement("h1");
+      h1.className = "mb-0";
+      logo.parentNode.insertBefore(h1, logo);
+      h1.appendChild(logo);
+    }
+  }
+});
+
+window.addEventListener("load", () => {
+  const hash = window.location.hash;
+
+  if (hash) {
+    // Espera o carregamento dos includes
+    const scrollToHash = () => {
+      const el = document.querySelector(hash);
+      if (el) {
+        el.scrollIntoView({ behavior: "smooth" });
+      } else {
+        // Tenta de novo em 100ms se ainda não existir
+        setTimeout(scrollToHash, 100);
+      }
+    };
+
+    scrollToHash();
+  }
+});
