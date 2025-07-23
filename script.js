86060d922e2a4d0f30af03c1f87708594a769338
Alberto Lima <alberto.btlima@outlook.com>
2025-07-23 12:03:20 -0300

    tratacao de erros no JS

--- script.js
+++ script.js
@@ -10,43 +10,49 @@ function isHomePage() {
 
 document.querySelectorAll("[data-include]").forEach(async (el) => {
   const file = el.getAttribute("data-include");
-  const res = await fetch(file);
-  const html = await res.text();
-  el.innerHTML = html;
+  try {
+    const res = await fetch(file);
+    if (!res.ok) throw new Error(`Erro ao carregar ${file}`);
+    const html = await res.text();
+    el.innerHTML = html;
 
-  // Carrossel
-  const carousels = el.querySelectorAll(".carousel");
-  carousels.forEach((carouselEl) => {
-    new bootstrap.Carousel(carouselEl, {
-      interval: 3000,
-      ride: "carousel",
-    });
-  });
-
-  // Reiniciar vídeos em todos os modais de vídeo
-  const modaisVideo = el.querySelectorAll(".modal[id^='modal-video-']");
-  modaisVideo.forEach((modal) => {
-    const iframe = modal.querySelector("iframe");
-    const originalSrc = iframe?.src || "";
-
-    modal.addEventListener("hidden.bs.modal", () => {
-      iframe.src = "";
+    // Carrossel
+    const carousels = el.querySelectorAll(".carousel");
+    carousels.forEach((carouselEl) => {
+      new bootstrap.Carousel(carouselEl, {
+        interval: 3000,
+        ride: "carousel",
+      });
     });
 
-    modal.addEventListener("show.bs.modal", () => {
-      iframe.src = originalSrc;
-    });
-  });
+    // Reiniciar vídeos em todos os modais de vídeo
+    const modaisVideo = el.querySelectorAll(".modal[id^='modal-video-']");
+    modaisVideo.forEach((modal) => {
+      const iframe = modal.querySelector("iframe");
+      const originalSrc = iframe?.src || "";
 
-  // <h1> Na home
-  if (isHomePage()) {
-    const logo = el.querySelector("#site-logo");
-    if (logo && !logo.closest("h1")) {
-      const h1 = document.createElement("h1");
-      h1.className = "mb-0";
-      logo.parentNode.insertBefore(h1, logo);
-      h1.appendChild(logo);
+      modal.addEventListener("hidden.bs.modal", () => {
+        iframe.src = "";
+      });
+
+      modal.addEventListener("show.bs.modal", () => {
+        iframe.src = originalSrc;
+      });
+    });
+
+    // <h1> Na home
+    if (isHomePage()) {
+      const logo = el.querySelector("#site-logo");
+      if (logo && !logo.closest("h1")) {
+        const h1 = document.createElement("h1");
+        h1.className = "mb-0";
+        logo.parentNode.insertBefore(h1, logo);
+        h1.appendChild(logo);
+      }
     }
+  } catch (err) {
+    console.error(err);
+    el.innerHTML = `<p class="text-danger">Erro ao carregar "${file}"</p>`;
   }
 });
 
